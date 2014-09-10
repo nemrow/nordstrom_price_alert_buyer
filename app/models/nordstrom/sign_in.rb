44aa31cc0742cc5473ff87c0beff59ef0d461263
Nemrow <nemrowj@gmail.com>
2014-09-10 11:51:11 -0700

    Sign In Functional with tests

--- app/models/nordstrom/sign_in.rb
+++ app/models/nordstrom/sign_in.rb
@@ -1,17 +1,38 @@
 module Nordstrom
   class SignIn
-    def initialize(browser)
+    def initialize(user, browser)
+      @user = user
       @browser = browser
     end
 
     def run
       go_to_signin_page
+      fill_in_data
+      submit_button.click
+      until @browser.li(:id => "shopper-status").exists? do sleep 1 end
     end
 
     private
       def go_to_signin_page
         signin_link = @browser.link(:text => "Sign In")
         signin_link.click
+      end
+
+      def fill_in_data
+        email_input.set(@user.nordstrom_email)
+        password_input.set(@user.nordstrom_password)
+      end
+
+      def email_input
+        @browser.text_field(:id => "ctl00_mainContentPlaceHolder_signIn_email")
+      end
+
+      def submit_button
+        @browser.link(:id => "ctl00_mainContentPlaceHolder_signIn_enterButton")
+      end
+
+      def password_input
+        @browser.text_field(:id => "ctl00_mainContentPlaceHolder_signIn_password")
       end
   end
 end
--- spec/models/nordstrom/sign_in_spec.rb
+++ spec/models/nordstrom/sign_in_spec.rb
@@ -1,7 +1,23 @@
 require 'spec_helper'
 
 describe Nordstrom::SignIn do
-   it "logs the user in" do
-      Nordstrom::SignIn.new(Nordstrom.browser).run
-   end
+
+  before(:each) do
+    @user = User.create(
+      :name => "Jordan Nemrow",
+      :email => ENV["JORDAN_NORDSTROM_EMAIL"],
+      :password => "password",
+      :nordstrom_email => ENV["JORDAN_NORDSTROM_EMAIL"],
+      :nordstrom_password => ENV["JORDAN_NORDSTROM_PASSWORD"],
+      :nordstrom_cc_cvc => ENV["JORDAN_NORDSTROM_CC_CVC_CODE"]
+    )
+    @browser = Nordstrom.browser
+  end
+
+  it "logs the user in" do
+    @browser.screenshot.save("test_1.png")
+    Nordstrom::SignIn.new(@user, @browser).run
+    @browser.screenshot.save("test_2.png")
+    assert_equal "Sign Out", @browser.li(:id => "shopper-status").text
+  end
 end
