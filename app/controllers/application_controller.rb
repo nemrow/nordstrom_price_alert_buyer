e588f03843075092ee6aa54c1788076bf0a9a00e
Nemrow <nemrowj@gmail.com>
2014-09-15 14:31:01 -0700

    Add basic flash alerts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  before_filter :display_flash_alerts
+
+  def display_flash_alerts
+    error = params[:error]
+    success = params[:success]
+    @flash = {:error => error, :success => success}
+  end
+
   def user_required
     if @user = User.find_by_id(session[:user_id]) || User.find_by_id(params[:user_id])
       return @user
